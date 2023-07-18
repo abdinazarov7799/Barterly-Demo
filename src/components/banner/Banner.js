a34dfa50e6e064d27f87855da73c70d61447980d
abdinazarov7799 <parolxato@gmail.com>
2023-07-18 18:53:41 +0500

    commit

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
       <>
             <Container>
                 <div className="banner">
-                    <h1>Welcome to e-Swap - Revolutionizing the Way You Trade!</h1>
+                    <h1>Welcome to Barterly - Revolutionizing the Way You Trade!</h1>
                     <p>
                         Our platform is designed to cater to your needs, whether you're a passionate collector, a fashion enthusiast, or simply looking to refresh
                         your belongings. With e-Swap, you can unlock a world of possibilities, connecting with like-minded individuals worldwide to trade
