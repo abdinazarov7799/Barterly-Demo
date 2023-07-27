2a8fc4d78b57cc1a46ccb05bb4fc46988d489d29
abdinazarov7799 <parolxato@gmail.com>
2023-07-28 04:02:48 +0500

    .

--- src/pages/Home/App.js
+++ src/pages/Home/App.js
@@ -3,7 +3,7 @@ import NavbarMenu from "../../Layouts/Navbar/Navbar";
 import Header from "../../Layouts/Header/Header";
 import Banner from "../../components/banner/Banner";
 import Categories from "../../components/categories/Categories";
-import HeroMenu from "../../components/hero-menu/HeroMenu";
+import HeroMenu from "../../components/Hero-menu/HeroMenu";
 import Footer from "../../Layouts/Footer/Footer";
 
 class App extends Component {
